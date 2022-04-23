fa06e82c761f21c208d781b893f7a5791b8c1ee6
Emeka Ikele <emeka.ikelee@gmail.com>
2022-04-23 13:09:59 +0100

    topbar done

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">emekaaadmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+            <span className="topIconBadge">2</span>
+          </div>
+          <img
+            src="https://www.linkpicture.com/q/IMG_20211030_124919.jpg"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
