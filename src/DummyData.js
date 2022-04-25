8542cba5de5e04f4e2ace2155128290ac363dd0e
Emeka Ikele <emeka.ikelee@gmail.com>
2022-04-25 03:09:33 +0100

    added reusable chart

---
+++ src/DummyData.js
@@ -0,0 +1,34 @@
+export const userData = [
+  {
+    name: "Monday",
+    "Active User": 4000,
+  },
+  {
+    name: "Monday",
+    "Active User": 7000,
+  },
+  {
+    name: "Tuesday",
+    "Active User": 3000,
+  },
+  {
+    name: "Wednesday",
+    "Active User": 2000,
+  },
+  {
+    name: "Thursday",
+    "Active User": 2780,
+  },
+  {
+    name: "Friday",
+    "Active User": 1890,
+  },
+  {
+    name: "Saturday",
+    "Active User": 2390,
+  },
+  {
+    name: "Sunday",
+    "Active User": 3190,
+  },
+];
